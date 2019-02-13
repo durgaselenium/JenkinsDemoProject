526e2309e80057736b922be3344cbbfac85f7737
admin <admin@admin-PC>
2019-02-13 21:55:48 +0530

    JenkinsDemoProject commit and pushed

---
+++ JenkinsDemoProject/src/demo/jenkins/JenkinsDemo1.java
@@ -0,0 +1,28 @@
+package demo.jenkins;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class JenkinsDemo1 {
+
+@Test	
+public void testDemo()
+{
+System.out.println("Hello Github");
+ 
+File file = new File("./Drivers/chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+WebDriver driver = new ChromeDriver();
+driver.get("https://learn-automation.com/");
+Assert.assertTrue(driver.getTitle().contains("Selenium"));
+
+driver.quit();
+
+}
+	
+}
